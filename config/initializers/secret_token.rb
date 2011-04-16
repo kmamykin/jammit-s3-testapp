b82beff7a5d74e9155d8cc2d7ce3fcf2fa180b0f
Kliment Mamykin <kmamyk@yahoo.com>
2011-04-16 17:12:10 -0400

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JammitS3Testapp::Application.config.secret_token = '45921370ee70dadfa25c31c72a3b86a655e72394216140bb89bd5d6b82e403eceebe2f40f196581a78ee2d52e767f0d62db4f9c0d320598225d28a7fbdcc5ee0'
